da5b5245c79ecdbbf2aba2776bf0e22e530288d2
Justin H Johnson <justin.hills.johnson@gmail.com>
2014-11-08 19:37:55 -0700

    Removing emulator aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -11,5 +11,3 @@ alias hosts="subl /private/etc/hosts"
 alias pg-start='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
 alias pg-stop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 alias dev="cd ~/Dropbox/_dev/"
-alias android="~/Dropbox/_dev/adt-bundle-mac-x86_64/sdk/tools/android"
-alias emulator="~/Dropbox/_dev/adt-bundle-mac-x86_64/sdk/tools/emulator @Test"
