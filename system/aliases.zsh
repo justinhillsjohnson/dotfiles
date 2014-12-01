629f74a8e884057c591bfc3fb0e132c87ebcf436
Justin H Johnson <justin.hills.johnson@gmail.com>
2014-12-01 10:06:45 -0700

    Updated indentation settings, alias PHP

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -11,3 +11,10 @@ alias hosts="subl /private/etc/hosts"
 alias pg-start='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
 alias pg-stop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 alias dev="cd ~/Dropbox/_dev/"
+
+# Simple Python server
+# Run as $ server
+alias server='python -m SimpleHTTPServer'
+
+# MAMP PHP v
+alias php='/Applications/MAMP/bin/php/php5.5.18/bin/php'
